36b860f37c22139cfc37bd1f3fdc03f734ba04db
Md Shoaib Islam <mestabd60@gmail.com>
2024-01-14 23:39:54 +0600

    update reverse array element

--- 1.cpp
+++ 1.cpp
@@ -5,37 +5,16 @@ int a[mx];
 int b[mx];
 int freq[mx];
 #define ll long long
-// int main(){
-//   int a[3][4], cnt =1;
-//   for(int i = 0; i < 3; i++){
-//     for(int j = 0; j < 4; j++){
-//       a[i][j] = cnt;
-//       cnt++;
-//     }
-//   }
-
-//   for(int i = 0; i < 3; i++){
-//     for(int j = 0; j < 4; j++){
-//       cout<<"(" <<i << " "<< j << ") "<< a[i][j]<< endl;
-//     }
-//     cout << endl;
-//   }
-// }
-
-
-#include <bits/stdc++.h>
-using namespace std;
 int main() {
-    int n;
+    int i,n;
+    ll a[100];
     cin >> n;
-    int ar[n];
-    for(int i = 0; i < n; i++) {
-        cin >> ar[i];
+    for(i=1;i<=n;i++)
+    {
+        cin >> a[i];
     }
-    int ans = ar[0];
-    for(int i = 1; i < n; i++) {
-        ans = max(ans, ar[i]);
+    for(i=n;i!=0;i--)
+    {
+        cout<< a[i]<<" ";
     }
-    cout << ans << endl;
-    return 0;
 }
