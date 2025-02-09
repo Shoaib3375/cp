d084244cfffa83109bfa7fc5c147ff6735fc8149
Md Shoaib Islam <mestabd60@gmail.com>
2025-02-10 01:09:46 +0600

    update

--- Programming/c++/practice/joti/recursion/DiceCombinations.cpp
+++ Programming/c++/practice/joti/recursion/DiceCombinations.cpp
@@ -1,28 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
-int solve(int sum)
-{
-    if (sum == 0)
-        return 1;
-    if (sum < 0)
-        return 0;
-
-    int res = 0;
-    for (int j = 1; j <= 6; j++)
+    #include <bits/stdc++.h>
+    using namespace std;
+    int solve(int sum)
     {
-        if (sum >= j)
+        if (sum == 0)
+            return 1;
+        
+
+        int res = 0;
+        for (int j = 1; j <= 6; j++)
         {
-            if (sum < 0)
-                return 0;
-            res += solve(sum - j);
+            if (sum >= j)
+            {
+                
+                res += solve(sum - j);
+            }
         }
+        return res;
     }
-    return res;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    int ans = solve(n);
-    cout << ans << endl;
-}
+    int main()
+    {
+        int n;
+        cin >> n;
+        int ans = solve(n);
+        cout << ans << '\n';
+    }
