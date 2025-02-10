50d90037f8957064fa50094956417ab5e864715a
Md Shoaib Islam <mestabd60@gmail.com>
2025-02-10 23:34:30 +0600

    update

--- Programming/c++/SOHAG/fibonacci.cpp
+++ Programming/c++/SOHAG/fibonacci.cpp
@@ -1,23 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int save[55];
+long long save[1000007];
 
-int fib(int n){
-    // if(n==0) return 0;
-    // if(n==1) return 1;
-    // if(save[n]!=-1) return save[n];
-    // save[n] = fib(n-1) + fib(n-2);
-    // return save[n];
-    if(n==0|| n==1) return 1;
-    else if(save[n] == 0){
-        save[n] = fib(n-1)+fib(n-2);
-    }
+long long fib(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    if(save[n]!=-1) return save[n];
+    save[n] = fib(n-1) + fib(n-2);
     return save[n];
+    // if(n==0|| n==1) return 1;
+    // else if(save[n] == 0){
+    //     save[n] = fib(n-1)+fib(n-2);
+    // }
+    // return save[n];
 
 }
 int main(){
     memset(save,-1,sizeof save);
-    int n = 40;
-    cout << fib(n)<<'\n';
+    int n;
+    cin>> n;
+    // cout << save[3]<<endl;
+    cout << (long long)fib(n)<<'\n';
 }
--- Programming/c++/practice/joti/recursion/DiceCombinations.cpp
+++ Programming/c++/practice/joti/recursion/DiceCombinations.cpp
@@ -1,12 +1,12 @@
     #include <bits/stdc++.h>
     using namespace std;
-    int solve(int sum)
+    long long  solve(long long sum)
     {
         if (sum == 0)
             return 1;
         
 
-        int res = 0;
+        long long res = 0;
         for (int j = 1; j <= 6; j++)
         {
             if (sum >= j)
@@ -21,6 +21,6 @@
     {
         int n;
         cin >> n;
-        int ans = solve(n);
+        long long ans = solve(n);
         cout << ans << '\n';
     }
