d8b4b82cccebbaaa17a87c3558d0eddcfe629f58
Md Shoaib Islam <mestabd60@gmail.com>
2025-04-27 23:02:53 +0600

    is prime

--- Programming/c++/Cps/Number Theory/isprime.cpp
+++ Programming/c++/Cps/Number Theory/isprime.cpp
@@ -4,19 +4,25 @@ using namespace std;
 
 bool isPrime(int n){
     if(n<=1) return false;
-    for (int i = 2; i*i <= n; i+=2)
+    if(n<=3) return true;
+    if(n%2==0) return false;
+    for (int i = 3; i*i <= n; i+=2)
     {
         if(n%i==0) return false;
     }
     return true;
 }
 int main(){
+    int t;
+    cin>> t;
+    while(t--){
     int n;
     cin >> n;
     if(isPrime(n)){
-        cout << "YES" << endl;
+        cout << "yes" << endl;
     }else{
-        cout << "NO" << endl;
+        cout << "no" << endl;
+    }
     }
     return 0;
 }
